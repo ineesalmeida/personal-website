5d32c9c4c2d9d942d6dc23bb47bdd0b1abd37204
Ines Almeida <ines.almeida@perspectum.com>
2021-01-15 23:58:49 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './css/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Inês Almeida
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
