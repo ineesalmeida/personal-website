de5bd5aebb9e76fd02d131f5c1946dbcc33eb316
guitarbeat <guitarbeats@me.com>
2022-04-10 20:41:07 -0500

    update

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function Header() {
         <div className='header'>
           <img src={process.env.PUBLIC_URL + '/profile.png'} className="header__picture" alt="me" />
           <div className="header__text">
-            <h1>Aaron</h1><h1> Woods</h1>
+            <h1>Aaron</h1><h1> Lorenzo</h1><h1> Woods</h1>
             <br />
             <h2>Biomedical Engineer</h2><h2> | </h2><h2>Artist</h2><h2> | </h2><h2>Scientist</h2>
             <br />
@@ -162,6 +162,14 @@ function Projects() {
       link: "https://opg.optica.org/ao/fulltext.cfm?uri=ao-59-34-10673&id=443588",
       content: "I contributed to this paper by writing MATLAB and C++ code that enabled the LCVR to be calibrated using a common-path interferometer.",
     },
+    {
+      title: "Data Collection for US Army NETCOM",
+      slug: 'Capston Project',
+      date: "2020",
+      keyword: "Misc",
+      link: "https://www.youtube.com/watch?v=3L-qHfODnI4",
+      content: "I was the project manager for this undergraduate senior capstone project. Our task was to improve a military data collection tool for network convergence. We used formative evaluation methods and usability testing to determine the best way to design the database and GUI.",
+    },
   ]
 
   let project_cards = projects.sort((a, b) => (a.date > b.date) ? -1 : 1).map((props) => <ProjectCard key={props.slug} {...props} />);
@@ -201,7 +209,7 @@ function Work() {
 
   let jobs = [
     {
-      'title': 'Graduate Student Researcher',
+      'title': 'Graduate Researcher',
       'company': 'Department of Biomedical Engineering | Advisor: Dr. Andrew Dunn',
       'place': 'Austin, TX',
       'from': "08-2021",
@@ -210,7 +218,16 @@ function Work() {
       'slug': 'persp-swe'
     },
     {
-      'title': 'Graduate Student Researcher',
+      'title': "Board Member",
+      'company': 'Executive Coordinating Committee | ICC Austin',
+      'place': 'Austin, TX',
+      'from': "08-2021",
+      'to': null,
+      'description': "The primary purpose of the Executive Coordinating Committee (ECC) is to provide leadership to – and coordinate the activities of - the Board as a whole to ensure excellence in fulfilling high-level governance duties.",
+      'slug': 'persp-swe'
+    },
+    {
+      'title': 'Graduate Researcher',
       'company': 'Department of Biomedical Engineering | Advisor: Dr. Mia Markey',
       'place': 'El Paso, TX',
       'from': "05-2021",
@@ -219,7 +236,7 @@ function Work() {
       'slug': 'persp-swe'
     },
     {
-      'title': 'Graduate Student Researcher',
+      'title': 'Graduate Researcher',
       'company': 'Department of Biomedical Engineering | Advisor: Dr. Evan Wang',
       'place': 'Austin, TX',
       'from': "08-2021",
@@ -228,7 +245,16 @@ function Work() {
       'slug': 'persp-swe'
     },
     {
-      'title': "Undergraduate Student Researcher",
+      'title': "Community Outreach Specialist",
+      'company': 'University of Texas at El Paso',
+      'place': 'El Paso, TX',
+      'from': "01-2020",
+      'to': "07-2020",
+      'description': "Develop comprehensive marketing programs for outreach/recruiting activities. Assist Program Manager in developing program curriculum and production of materials. Design methods that evaluate program success, making adjustments as appropriate quantifiably. Visit outreach sites and conduct program education and exhibit presentations to audiences",
+      'slug': 'persp-swe'
+    },
+    {
+      'title': "Undergraduate Researcher",
       'company': 'CREaTE Lab | Advisor: Dr. Peter Golding',
       'place': 'El Paso, TX',
       'from': "08-2019",
@@ -237,7 +263,7 @@ function Work() {
       'slug': 'persp-swe'
     },
     {
-      'title': "Undergraduate Student Researcher",
+      'title': "Undergraduate Researcher",
       'company': 'Adult Neurology Ketogenic Diet Therapy Clinic | Advisor: Dr. Elizabeth Felton',
       'place': 'Madison, WI',
       'from': "05-2019",
@@ -246,12 +272,21 @@ function Work() {
       'slug': 'persp-swe'
     },
     {
-      'title': "Undergraduate Student Researcher",
+      'title': "Undergraduate Researcher",
       'company': 'Biomedical Optics Lab | Advisor: Dr. Jeremy Rogers',
       'place': 'Madison, WI',
       'from': "05-2018",
       'to': "08-2018",
       'description': "Constructed an interferometer to measure phase delay.Programmed an automated calibration software for optical equipment. Inspected laboratory standards and practices for maintaining lab.",
+      'slug': 'persp-swe'
+    },
+    {
+      'title': "Amazon Prime Student Ambassador",
+      'company': 'Riddle and Bloom | Amazon',
+      'place': 'El Paso, TX',
+      'from': "08-2017",
+      'to': "10-2018",
+      'description': "Increased brand awareness through coordinated events averaging 300+ student interactions. Developed and implemented digital, experiential, and peer-to-peer tactics for student events. Collaborated in positive interpersonal relationships within the school community",
       'slug': 'persp-swe'
     },
     {
@@ -264,39 +299,30 @@ function Work() {
       'slug': 'persp-swe'
     },
     {
-      'title': "Board Member",
-      'company': 'Executive Coordinating Committee | ICC Austin',
-      'place': 'Austin, TX',
-      'from': "08-2021",
-      'to': null,
-      'description': "",
-      'slug': 'persp-swe'
-    },
-    {
-      'title': "Community Outreach Specialist",
-      'company': 'University of Texas at El Paso',
-      'place': 'El Paso, TX',
-      'from': "01-2020",
-      'to': "07-2020",
-      'description': "",
-      'slug': 'persp-swe'
-    },
-    {
       'title': "Project Analyst",
       'company': 'University of Texas at El Paso',
       'place': 'El Paso, TX',
       'from': "08-2018",
-      'to': "012-2019",
-      'description': "",
+      'to': "12-2020",
+      'description': "Extract and illustrate retention rates via visualization of historical enrollment data. Assist with in-person, online, and telephone inquiries for academic department operations. Iterative design of the department’s advising process and promotion for 200+ students. Mentorship of student staff for sustainability in future department data management and reporting",
       'slug': 'persp-swe'
     },
     {
-      'title': "",
-      'company': '',
+      'title': "Sound Engineering Technician",
+      'company': 'Beacon Hill Recording Studios',
       'place': 'El Paso, TX',
-      'from': "",
-      'to': "",
-      'description': "",
+      'from': "10-2018",
+      'to': "08-2019",
+      'description': "Provided support in microphone position and placement for recording sound. Verified signal-flow patch bay configurations and outboard gear for editing and mixing. Extensive use of industry-standard DAWs, such as ProTools and Ableton for editing and mixing",
+      'slug': 'persp-swe'
+    },
+    {
+      'title': "Financial Project Engineer",
+      'company': 'Sonora Bank',
+      'place': 'Boerne, TX',
+      'from': "05-2017",
+      'to': "08-2017",
+      'description': "Designed and formatted a personal financial statement for customer use. Practiced daily responsibilities from several departments, ranging from tellers to CFO. Trained in administering effective group meetings and presentations.",
       'slug': 'persp-swe'
     },
   ];
