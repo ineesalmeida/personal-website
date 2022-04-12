f33a08b578a20ec4749773770aef3eabadf664e7
guitarbeat <guitarbeats@me.com>
2022-04-11 21:22:02 -0500

    update

--- src/App.js
+++ src/App.js
@@ -208,11 +208,11 @@ function Work() {
   let jobs = [
     {
       'title': 'Graduate Researcher',
-      'company': 'Department of Biomedical Engineering | Advisor: Dr. Andrew Dunn',
+      'company': 'Functional Optical Imaging Laboratory | Advisor: Dr. Andrew Dunn',
       'place': 'Austin, TX',
       'from': "08-2021",
       'to': null,
-      'description': "Developing microvasculature optical imaging techniques. Investigating digital image processing and quantification. Training on surgical craniotomies for biomedical imaging",
+      'description': "This is what I'm doing right now. I'm currently working on a project that will improve our understanding of how new blood vessels form, which could lead to new insights into stroke recovery and other diseases.",
       'slug': 'persp-swe'
     },
     {
@@ -221,61 +221,52 @@ function Work() {
       'place': 'Austin, TX',
       'from': "08-2021",
       'to': null,
-      'description': "The primary purpose of the Executive Coordinating Committee (ECC) is to provide leadership to – and coordinate the activities of - the Board as a whole to ensure excellence in fulfilling high-level governance duties.",
-      'slug': 'persp-swe'
-    },/*
-    {
-      'title': 'Graduate Researcher',
-      'company': 'Department of Biomedical Engineering | Advisor: Dr. Mia Markey',
-      'place': 'El Paso, TX',
-      'from': "05-2021",
-      'to': "08-2021",
-      'description': "Design of decision support system for breast cancer patients",
+      'description': "Here, I help provide leadership to the board of directors of ICC Austin, a non-profit organization of student co-op housing, and I help coordinate the Board's activities as a whole to ensure that high-level governance duties are met.",
       'slug': 'persp-swe'
     },
     {
-      'title': 'Graduate Researcher',
-      'company': 'Department of Biomedical Engineering | Advisor: Dr. Evan Wang',
-      'place': 'Austin, TX',
-      'from': "08-2021",
+      'title': 'Rotation Student',
+      'company': 'UT Biomedical Informatics Lab | Wang Research Group',
+      'place': 'El Paso, TX',
+      'from': "05-2021",
       'to': "10-2021",
-      'description': "Conducted EEG data acquisition and analysis. Created SOP for fabricating dry electrodes for long recordings",
+      'description': "During my first year rotations, I worked on the development of a decision support system for breast cancer patients, the acquisition and analysis of EEG data, and the fabrication of dry electrodes for long recordings.",
       'slug': 'persp-swe'
-    },*/
+    },
     {
       'title': "Community Outreach Specialist",
       'company': 'University of Texas at El Paso',
       'place': 'El Paso, TX',
-      'from': "01-2020",
+      'from': "08-2018",
       'to': "07-2020",
-      'description': "Develop comprehensive marketing programs for outreach/recruiting activities. Assist Program Manager in developing program curriculum and production of materials. Design methods that evaluate program success, making adjustments as appropriate quantifiably. Visit outreach sites and conduct program education and exhibit presentations to audiences",
+      'description': "Here, I created marketing programs and materials for student outreach, and I collaborated with the department chair to create the business acumen curriculum for a four-year B.S. program in engineering innovation and leadership. I used data visualization to assess the program's retention and recruitment health and then presented that information to make data-driven decisions.",
       'slug': 'persp-swe'
     }, /*
     {
       'title': "Undergraduate Researcher",
       'company': 'CREaTE Lab | Advisor: Dr. Peter Golding',
       'place': 'El Paso, TX',
-      'from': "08-2019",
+      'from': "08-2018",
       'to': "12-2020",
       'description': "Innovated on engineering education regarding ethics and leadership. Programmed advising software to assist with course recommendation. Optimized the advising process to better falicitate faculty and student",
       'slug': 'persp-swe'
     },*/
     {
-      'title': "UG Research in Clinical Data Science",
+      'title': "Clinical UG Researcher",
       'company': 'Adult Neurology Ketogenic Diet Therapy Clinic | Advisor: Dr. Elizabeth Felton',
       'place': 'Madison, WI',
       'from': "05-2019",
       'to': "08-2019",
-      'description': "Data visualization of patient data for clinical and dietary analysis. Managed HIPAA compliance, safety, and ethics for patient research. Shadowed doctor-patient relationship and treatment of epilepsy.",
+      'description': "In a hospital, I worked in a small lab to create data visualizations of patient data for clinical and dietary analysis and decision making, managed HIPAA compliance, safety, and ethics for patient research, and shadowed the doctor-patient relationship and epilepsy treatment.",
       'slug': 'persp-swe'
     },
     {
-      'title': "UG Research in Optics and Image Processing",
+      'title': "Optical UG Researcher",
       'company': 'Biomedical Optics Lab | Advisor: Dr. Jeremy Rogers',
       'place': 'Madison, WI',
       'from': "05-2018",
       'to': "08-2018",
-      'description': "Constructed an interferometer to measure phase delay.Programmed an automated calibration software for optical equipment. Inspected laboratory standards and practices for maintaining lab.",
+      'description': "This was my first exposure to biomedical engineering research, and it was the catalyst for me to pursue a Ph.D. I was working on an interferometer to measure phase delay here. I also wrote automated calibration software for optical equipment in a language I didn't even understand at the time!",
       'slug': 'persp-swe'
     },
     {
@@ -284,7 +275,7 @@ function Work() {
       'place': 'El Paso, TX',
       'from': "08-2017",
       'to': "10-2018",
-      'description': "Increased brand awareness through coordinated events averaging 300+ student interactions. Developed and implemented digital, experiential, and peer-to-peer tactics for student events. Collaborated in positive interpersonal relationships within the school community",
+      'description': "I worked to represent and raise awareness of this brand through a series of coordinated events, averaging 300+ student interactions. It assisted me in becoming more comfortable with planning student events and networking.",
       'slug': 'persp-swe'
     },
     {
@@ -293,7 +284,7 @@ function Work() {
       'place': 'El Paso, TX',
       'from': "01-2018",
       'to': "05-2018",
-      'description': "Coordinated the trainings of students to use a fabrication laboratory. Taught procedures on computer-aided design and simulation. Directed the development process to build working prototypes.",
+      'description': "I coordinated student trainings to use a fabrication laboratory, taught computer-aided design and simulation procedures, and directed the development process to build working prototypes.",
       'slug': 'persp-swe'
     },/*
     {
@@ -311,7 +302,7 @@ function Work() {
       'place': 'El Paso, TX',
       'from': "10-2018",
       'to': "08-2019",
-      'description': "Provided support in microphone position and placement for recording sound. Verified signal-flow patch bay configurations and outboard gear for editing and mixing. Extensive use of industry-standard DAWs, such as ProTools and Ableton for editing and mixing",
+      'description': "I got a firsthand look at what it's like to work at a renowned music studio that has worked with artists such as Khalid, Kali Uchis, and French Kiwi Juice. I assisted with the recording process and learned about how songs are mixed and mastered in the music industry.",
       'slug': 'persp-swe'
     },
     {
@@ -320,7 +311,7 @@ function Work() {
       'place': 'Boerne, TX',
       'from': "05-2017",
       'to': "08-2017",
-      'description': "Designed and formatted a personal financial statement for customer use. Practiced daily responsibilities from several departments, ranging from tellers to CFO. Trained in administering effective group meetings and presentations.",
+      'description': "I designed and formatted a personal financial statement for customer use, practiced daily responsibilities from tellers to the CFO, and most importantly, I learned good money management.",
       'slug': 'persp-swe'
     },
   ];
